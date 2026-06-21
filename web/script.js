4172fcae6900dc04ae6b6c3a403570f3a8a8288a
virtualsundar <163748495+virtualsundar@users.noreply.github.com>
2026-06-21 11:51:44 +0545

    Add files via upload

---
+++ web/script.js
@@ -0,0 +1,8 @@
+// Smooth scroll
+document.querySelectorAll('a[href^="#"]').forEach(a => {
+  a.addEventListener("click", e => {
+    e.preventDefault();
+    document.querySelector(a.getAttribute("href"))
+      .scrollIntoView({ behavior: "smooth" });
+  });
+});
